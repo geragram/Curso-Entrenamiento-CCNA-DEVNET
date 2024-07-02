daea6bf40bafb272a4a6fd6f5938a62684ea8fbf
NetAcad DEVASC <changeme@example.com>
2024-07-02 02:06:39 +0000

    Añadido scrpt de datos personales

---
+++ datos.py
@@ -0,0 +1,13 @@
+def main():
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("Ingrese su apellido: ")
+    edad = input("Ingrese su edad: ")
+    sede = input("Ingrese su sede: ")
+
+    print(f"Nombre: {nombre}")
+    print(f"Apellido: {apellido}")
+    print(f"Edad: {edad}")
+    print(f"Sede: {sede}")
+
+if __name__ == "__main__":
+    main()
